d50bb51ebc73717361049364b1434c6fc40e3a43
alexeytimko <alexeytimko@gmail.com>
2018-05-31 14:23:08 +0300

    initial

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://165.227.135.155:8000';
+const baseURL = 'http://test-me.com/api';
 
 export default {
     saveTest: data => axios.post(`${baseURL}/test`, data)
